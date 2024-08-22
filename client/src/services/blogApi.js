03bec0494f6d179c8a53fd9de420a26abe6fb3cf
Safwan Backar <safwanbackar29@gmail.com>
2024-08-22 20:01:24 +0530

    axios blog api fix

--- client/src/services/blogApi.js
+++ client/src/services/blogApi.js
@@ -5,6 +5,6 @@ import { blogApi } from '../api/endpoints';
 export const getBlogs = () => axios.get(`${blogApi}`);
 export const getBlogById = (id) => axios.get(`${blogApi}/${id}`);
 export const getBlogsByUserId = (userId) => axiosInstanceGetBlog.get(`/user/${userId}`);
-export const createBlog = (blogData) => axiosInstanceBlogCUD.post(`/create`, blogData)
-export const updateBlog = (id, blogData) => axiosInstanceBlogCUD.put(`/${id}/edit`, blogData)
-export const deleteBlog = (id) => axiosInstanceBlogCUD.delete(`/${id}`)
+export const createBlog = (blogData) => axios.post(`${blogApi}/create`, blogData)
+export const updateBlog = (id, blogData) => axios.put(`${blogApi}/${id}/edit`, blogData)
+export const deleteBlog = (id) => axios.delete(`${blogApi}/${id}`)
