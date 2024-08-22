6bcf53651399a551d28653daacef3f265a97a77b
Safwan Backar <safwanbackar29@gmail.com>
2024-08-22 19:28:32 +0530

    axios auth fix

--- client/src/services/authApi.js
+++ client/src/services/authApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { axiosInstaceAuth } from '../api/axios';
 
-export const handleLogin = (formData) => axios.post('https://blog-app-mern-production.up.railway.app/login', formData)
-export const handleSignUp = (formData) => axiosInstaceAuth.post('/register', formData)
-export const handleLogout = async ()=> axiosInstaceAuth.post('/logout')
+export const handleLogin = (formData) => axios.post('https://blog-app-mern-production.up.railway.app/auth/login', formData)
+export const handleSignUp = (formData) => axios.post('https://blog-app-mern-production.up.railway.app/auth/register', formData)
+export const handleLogout = async ()=> axiosInstaceAuth.post('https://blog-app-mern-production.up.railway.app/auth/logout')
