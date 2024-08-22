c843c0aced3ce951fbea3d000a94f34a7cd2389d
Safwan Backar <safwanbackar29@gmail.com>
2024-08-22 19:05:53 +0530

    axios instane check

--- client/src/services/authApi.js
+++ client/src/services/authApi.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import { axiosInstaceAuth } from '../api/axios';
 
-export const handleLogin = (formData) => axiosInstaceAuth.post('/login', formData)
+export const handleLogin = (formData) => axios.post('https://blog-app-mern-production.up.railway.app/login', formData)
 export const handleSignUp = (formData) => axiosInstaceAuth.post('/register', formData)
 export const handleLogout = async ()=> axiosInstaceAuth.post('/logout')
--- client/src/services/blogApi.js
+++ client/src/services/blogApi.js
@@ -1,6 +1,7 @@
+import axios from 'axios';
 import  { axiosInstanceBlogCUD, axiosInstanceGetBlog } from '../api/axios';
 
-export const getBlogs = () => axiosInstanceGetBlog.get('/all-blogs');
+export const getBlogs = () => axios.get('https://blog-app-mern-production.up.railway.app/blogs');
 export const getBlogById = (id) => axiosInstanceGetBlog.get(`/${id}`);
 export const getBlogsByUserId = (userId) => axiosInstanceGetBlog.get(`/user/${userId}`);
 export const createBlog = (blogData) => axiosInstanceBlogCUD.post(`/create`, blogData)
