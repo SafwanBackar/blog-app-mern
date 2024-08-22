a80e4eb94d966cbf7a21e9f6abe592793a752a55
Safwan Backar <safwanbackar29@gmail.com>
2024-08-22 20:36:18 +0530

    build headers auth

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -1,2 +1,8 @@
 export const authApi = 'https://blog-app-mern-production.up.railway.app/auth'
 export const blogApi = 'https://blog-app-mern-production.up.railway.app/blogs'
+export const headersBlog = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    }
+}
--- client/src/services/blogApi.js
+++ client/src/services/blogApi.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import  { axiosInstanceGetBlog } from '../api/axios';
 import { blogApi } from '../api/endpoints';
+import { headersBlog } from '../api/endpoints';
 
 export const getBlogs = () => axios.get(`${blogApi}`);
 export const getBlogById = (id) => axios.get(`${blogApi}/${id}`);
 export const getBlogsByUserId = (userId) => axiosInstanceGetBlog.get(`/user/${userId}`);
-export const createBlog = (blogData) => axios.post(`${blogApi}/create`, blogData)
-export const updateBlog = (id, blogData) => axios.put(`${blogApi}/${id}/edit`, blogData)
-export const deleteBlog = (id) => axios.delete(`${blogApi}/${id}`)
+export const createBlog = (blogData) => axios.post(`${blogApi}/create`, blogData, headersBlog)
+export const updateBlog = (id, blogData) => axios.put(`${blogApi}/${id}/edit`, blogData, headersBlog)
+export const deleteBlog = (id) => axios.delete(`${blogApi}/${id}`, headersBlog)
